854aca1dec06f3abdccf3476477948fd7a16d5e5
osoyliz <lizosoy18@gmail.com>
2018-07-10 17:45:18 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -16,6 +16,7 @@ end
 lunch("chicken")
 def dinner(food= "salmon")
   puts "many people like #{food}"
+  dinner("fries")
 # Call the methods with puts and your own arguments here. Like this:
 puts snacks("Swedish fish")
 ## This returns "Any time, is the right time for Sweedish fish!"
